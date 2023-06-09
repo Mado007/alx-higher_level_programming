19c62a0104ed78d7d70cf889dffbe8b49c34580e
Mado007 <mahmodeid2019@gmail.com>
2023-06-08 17:09:59 -0700

    Hot fix variables

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-# Author - Mado007
 
 if __name__ == "__main__":
-  
+    """Print  sum 1 and 2."""
     from add_0 import add
 
-    a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    a = 1
+    print("{} + {} = {}".format(b, a, add(b, a)))
