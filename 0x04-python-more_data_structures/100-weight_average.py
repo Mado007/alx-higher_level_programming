fcd6e0fcf546279bdf15b792c4690c0e335c69d3
Mahmoud.Eid <39015047+Mado007@users.noreply.github.com>
2023-06-14 22:06:49 +0300

    Update 100-weight_average.py
    Fix Int

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,12 +4,12 @@ def weight_average(my_list=[]):
     if not my_list:
         return 0
 
-    dend = 0
-    numo = 0
+    den = 0
+    num = 0
     
 
     for tup in my_list:
-        dend += tup[1]
-        numo += tup[0] * tup[1]
+        den += tup[1]
+        num += tup[0] * tup[1]
 
     return (num / den)
