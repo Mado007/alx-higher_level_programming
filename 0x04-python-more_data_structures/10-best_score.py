9ae6514c99352b040f2e9eabcbbf10bbf549fa8f
Mado007 <mahmodeid2019@gmail.com>
2023-06-14 12:29:58 -0700

    fix py style

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -11,11 +11,11 @@ def best_score(a_dictionary):
 
         leader = ""
         score = 0
-        
+
         for i in my_list:
             if a_dictionary[i] > score:
 
                 leader = i
                 score = a_dictionary[i]
-                
+
         return leader
