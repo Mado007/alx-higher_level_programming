c2848f9085113b7f389148e4abff8821124a498e
Mado007 <mahmodeid2019@gmail.com>
2023-06-15 17:00:25 -0700

    update dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    """
+    function that replace or add
+    key or value in a diction.
+    """
+    a_dictionary[key] = value
+    return a_dictionary
