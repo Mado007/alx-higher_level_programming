994c1a2bc3a1f826ed4a1455d69c914b6442b30b
Mado007 <mahmodeid2019@gmail.com>
2023-10-01 18:43:28 +0300

    fix port

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # script makes request to curl url till we get "You got me! in the body of the response.
 curl -sX PUT -L -d "user_id=98" --header "origin: HolbertonSchool" 0.0.0.0:5000/catch_me
-0
